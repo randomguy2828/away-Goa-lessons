87bd471acac106f87631ab2105a97d44af8ff89c
randomguy2828 <Gorgogaduma@gmail.com>
2024-07-15 08:31:01 -0700

    Add files via upload

---
+++ level 10/classwork/classwork3.py
@@ -0,0 +1,22 @@
+
+    
+counter=0
+
+while counter<100:
+    print(counter)
+    counter=counter+1
+
+
+counter=0
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ level 10/homework/homework.py
@@ -0,0 +1,26 @@
+
+for i in range(10):
+    print(i)
+    print("hello world")
+
+for i in range(0, 1000, 100):
+    print(i)
+
+for i in range(100, 0, -1):
+    print(i)
+
+counter=0
+while counter < 10:
+    print(counter)
+    print("hello")
+    counter=counter+1
+
+num=0
+while num < 1000:
+    print(num)
+    num=num + 100
+
+num1=100
+while num1 > 0:
+    print(num1)
+    num1=num1-1
